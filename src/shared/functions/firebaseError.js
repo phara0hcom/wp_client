62be62208b001e70c8164a4b039595ba0693477e
phara0hcom <t.elsay3d@gmail.com>
2019-11-18 14:10:09 +0900

    error parsing

---
+++ src/shared/functions/firebaseError.js
@@ -0,0 +1,19 @@
+export const authErrorObj = {
+  "auth/app-deleted": "Please contact the websites Administrator",
+  "auth/app-not-authorized": "Please contact the websites Administrator",
+  "auth/argument-error": "Please contact the websites Administrator",
+  "auth/invalid-api-key": "Please contact the websites Administrator",
+  "auth/invalid-user-token": "Invalid Token",
+  "auth/invalid-tenant-id": "Invalid Id",
+  "auth/network-request-failed": "Network Error",
+  "auth/operation-not-allowed": "This operation is not allowed",
+  "auth/requires-recent-login": "Please login first",
+  "auth/too-many-requests": "There are to many request, please try again later",
+  "auth/unauthorized-domain": "Please contact the websites Administrator",
+  "auth/user-disabled": "This account has been disabled",
+  "auth/user-token-expired": "User token has expired",
+  "auth/web-storage-unsupported": "This browser is not supported",
+  "auth/invalid-email": "The entered email is invalid",
+  "auth/user-not-found": "User is not found please sign-up",
+  "auth/wrong-password": "The entered password is incorrect"
+};
--- src/store/actions/login.js
+++ src/store/actions/login.js
@@ -1,6 +1,7 @@
 import * as actionsTypes from "../actions/actionTypes";
 import { getCookie } from "../../shared/functions/utils";
 import firebase from "../../shared/firebase";
+import { authErrorObj } from "../../shared/functions/firebaseError";
 
 export const getAuthorization = () => dispatch => {
   //call backend to validate token
@@ -27,9 +28,16 @@ export const loginUser = (email, password) => dispatch => {
     .signInWithEmailAndPassword(email, password)
     .then(res => {
       console.log({ res });
+      const user = {
+        uid: res.user.uid,
+        displayName: res.user.displayName,
+        email: res.user.email
+      };
+      // make a getUser call for the rest of the info
+      // make a new get user call for only the userDB
     })
-    .catch(error => {
-      console.log({ error });
-      dispatch({ type: actionsTypes.LOGIN_ERROR, error: error.message });
+    .catch(err => {
+      const error = authErrorObj[err.code];
+      dispatch({ type: actionsTypes.LOGIN_ERROR, error, fullError: err });
     });
 };
