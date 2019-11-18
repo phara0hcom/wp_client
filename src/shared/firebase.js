db1fec41f0a6a3ea2fc1a9cc08c65c03a084d2fc
phara0hcom <t.elsay3d@gmail.com>
2019-11-18 13:44:59 +0900

    fixed deploy

--- src/shared/firebase.js
+++ src/shared/firebase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import firebaseConfig from "../../firebaseConfig";
+import firebaseConfig from "../firebaseConfig";
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
